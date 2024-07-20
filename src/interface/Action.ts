8707a04ef2d60c38add6fd1380b8ca04a137666e
minhyeong <minhyeong.dev@gmail.com>
2024-07-21 05:02:56 +0900

    [Feat] Controller component for action whatever

---
+++ src/interface/Action.ts
@@ -0,0 +1,20 @@
+export interface ActionEvent {
+  event: string;
+  type: "view" | "modal";
+}
+
+type Action<T extends boolean | string | null | undefined> = [
+  T,
+  React.ReactNode
+];
+export type Show = Action<boolean | string>;
+export type Replace = Action<boolean | string | null | undefined>;
+export interface ShowAction {
+  event: string;
+  isVisible: boolean;
+}
+
+export interface ActionProps {
+  replaces?: Replace[];
+  shows?: Show[];
+}
