6d90d2d3ddfe14b0a4b0acdcf95ec806dad97804
minhyeong <minhyeong.dev@gmail.com>
2024-07-21 05:02:27 +0900

    [Feat] useful properties

---
+++ src/interface/Property.ts
@@ -0,0 +1,18 @@
+import { Dispatch, SetStateAction } from "react";
+
+export type StateAction<T> = Dispatch<SetStateAction<T>>;
+export type State<T> = [T, StateAction<T>];
+
+export type Click<REQ = unknown, RES = any> = (prop?: REQ) => RES;
+export type OnClick<Request = unknown, Response = unknown> = (
+  prop?: Request
+) => Response | ((prop?: Request) => Promise<Response>);
+
+export type Titles = {
+  title: string;
+  subtitle?: string | string[];
+  isSub?: boolean;
+  color?: string;
+  titleColor?: string;
+  subtitleColor?: string;
+};
